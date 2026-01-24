1719c804516ba64c776f2cbca11ea97d12df875a
ehemAushilfskassierer <ehem.Aushilfskassierer@gmail.com>
2026-01-24 19:22:26 +0100

    Update d_004_Challenging_BinaryConversion.py

--- PythonBasics/Challenges/d_004_Challenging_BinaryConversion.py
+++ PythonBasics/Challenges/d_004_Challenging_BinaryConversion.py
@@ -4,8 +4,19 @@
 Binary Conversion
 Use bin() to convert 12 into binary.
 """
+
 intValue = 12
 
 print(intValue)
 
 print(f"{bin(intValue) = }")
+
+print(f"{int(bin(intValue), 2) = }")
+
+a, b = 8, 9
+
+print(f"{bin(a) = }\t{bin(b) = }")
+
+print(f"{bin(a & b) = }\t{a & b = }")
+
+print(f"{bin(a | b) = }\t{a | b = }")
